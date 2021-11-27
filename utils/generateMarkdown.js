cd0917f7e8afcdbc0008e7d8a2fea698bd16d9db
Janette <janette.castillo91@yahoo.com>
2021-11-27 17:23:05 -0600

    created function for license choice

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "MIT") {
+    return '![License: MIT](<https://img.shields.io/badge/License-MIT-yellow.svg)'
+  } else if (license === 'Apache 2.0') {
+    return '![License: Apache 2.0.](<https://img.shields.io/badge/License-Apache_2.0-blu.svg>)'
+  } else {
+    return ''
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
