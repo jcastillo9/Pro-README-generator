d1e188013c90bb37c73697858d2a1db4826eec95
Janette <janette.castillo91@yahoo.com>
2021-11-27 22:28:19 -0600

    Added array for licenses

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,40 +1,51 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+const licenseOptions = ["MIT", "Academic Free License v3.0", "Apache license 2.0", "Artistic license 2.0"] 
+
+// function that returns a license badge based on which license is passed in
 function renderLicenseBadge(license) {
-  if (license === 'MIT') {
+  if (license === licenseOptions[0]) {
     return '![License: MIT](<https://img.shields.io/badge/License-MIT-yellow.svg>)'
-  } else if (license === 'Academic Free License v3.0') {
+  } else if (license === licenseOptions[1]) {
     return '[![License: AFL-3.0](<https://img.shields.io/badge/License-AFL--3.0-lightgrey.svg>)'
-  } else if (license === 'Apache 2.0') {
+  } else if (license === licenseOptions[2]) {
     return '![License: Apache 2.0.](<https://img.shields.io/badge/License-Apache_2.0-blu.svg>)'
-  } else if (license === 'Artistic license 2.0') {
+  } else if (license === licenseOptions[3]) {
     return '![License: Artistic-2.0](<https://img.shields.io/badge/License-Artistic%202.0-0298c3.svg>)'
   } else {
     return ''
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+// function that returns the license link
 function renderLicenseLink(license) {
-  if (license === 'MIT') {
+  if (license === licenseOptions[0]) {
     return '[MIT](https://opensource.org/licenses/MIT)'
-  } else if (license === 'Academic Free License v3.0') {
+  } else if (license === licenseOptions[1]) {
     return '[AFL-3.0]((https://opensource.org/licenses/AFL-3.0))'
-  } else if (license === 'Apache 2.0') {
+  } else if (license === licenseOptions[2]) {
     return '[Apache 2.0.](https://opensource.org/licenses/Apache-2.0)'
-  } else if (license === 'Artistic license 2.0') {
+  } else if (license === licenseOptions[3]) {
     return '[Artistic-2.0](https://opensource.org/licenses/Artistic-2.0)'
   } else {
     return ''
   }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// function that returns the license section of README
+function renderLicenseSection(license) {
+  if (license === 'MIT') {
+    return `Read more about ${licenseOptions[0]} here:`
+  } else if (license === 'Academic Free License v3.0') {
+    return `Read more about ${licenseOptions[1]} here:`
+  } else if (license === 'Apache 2.0') {
+    return `Read more about ${licenseOptions[2]} here:`
+  } else if (license === 'Artistic license 2.0') {
+    return `Read more about ${licenseOptions[3]} here:`
+  } else {
+    return ''
+  }
+}
 
-// TODO: Create a function to generate markdown for README
+// function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
   ## Badges
