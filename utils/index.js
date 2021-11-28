6701398cb3c73053dd03494ba5602daf5c43ee15
Janette <janette.castillo91@yahoo.com>
2021-11-27 23:45:32 -0600

    debug

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,7 @@
 // packages needed for this project
 const inquirer = require('inquirer');
 const fs = require('fs')
-const generateMarkdown = require('./utils/generateMarkdown')
+const generateMarkdown = require('./generateMarkdown')
 
 // array of questions for user input
 const questions = [{
@@ -47,7 +47,7 @@ const questions = [{
 // TODO: Create a function to write README file
 function writeToFile(data) {
     const writeReadme = generateMarkdown(data);
-    fs.writeFile('README.md', writeReadme, (err) =>
+    fs.writeFile('SAMPLEREADME.md', writeReadme, (err) =>
     err ? console.log(err) : console.log('Created your README.md')
     );
 
